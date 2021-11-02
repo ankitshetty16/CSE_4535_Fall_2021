b4de52ad774ed48b24c8a0e7f511ea9d5229f517
ankitshetty16 <ankitshetty16@gmail.com>
2021-11-02 12:56:26 -0400

    Project 3 initial commit

---
+++ project3/replace_bm25.py
@@ -0,0 +1,214 @@
+import os
+import pysolr
+import requests
+import json
+
+CORE_NAME = "IRF21_p3_bm25_1"
+AWS_IP = "13.58.228.187"
+
+
+def delete_core(core=CORE_NAME):
+    print(os.system('sudo su - solr -c "/opt/solr/bin/solr delete -c {core}"'.format(core=core)))
+
+
+def create_core(core=CORE_NAME):
+    print(os.system(
+        'sudo su - solr -c "/opt/solr/bin/solr create -c {core} -n data_driven_schema_configs"'.format(
+            core=core)))
+
+class Indexer:
+    def __init__(self):
+        self.solr_url = f'http://{AWS_IP}:8983/solr/'
+        self.connection = pysolr.Solr(self.solr_url + CORE_NAME, always_commit=True, timeout=5000000)
+
+    def do_initial_setup(self):
+        delete_core()
+        create_core()
+
+    def create_documents(self, docs):
+        print(self.connection.add(docs))
+
+    def add_fields(self):
+        data = {
+            "add-field": [
+                {
+                    "name": "lang",
+                    "type": "string",
+                    "indexed": True,
+                    "multiValued": False
+                },
+                {
+                    "name": "text_de",
+                    "type": "text_de",
+                    "indexed": True,
+                    "multiValued": False
+                }, {
+                    "name": "text_en",
+                    "type": "text_en",
+                    "indexed": True,
+                    "multiValued": False
+                },
+                {
+                    "name": "text_ru",
+                    "type": "text_ru",
+                    "indexed": True,
+                    "multiValued": False
+                },
+                {
+                    "name": "tweet_urls",
+                    "type": "strings",
+                    "indexed": True,
+                    "multiValued": True
+                },
+                {
+                    "name": "tweet_hashtags",
+                    "type": "strings",
+                    "indexed": True,
+                    "multiValued": True
+                },
+            ]
+        }
+
+        print(requests.post(self.solr_url + CORE_NAME + "/schema", json=data).json())
+
+    def replace_fields(self):
+        data = {
+            "replace-field": [
+                {
+                    "name": "age",
+                    "type": "string",
+                    "multiValued": False
+                }
+            ]
+        }
+
+        print(requests.post(self.solr_url + CORE_NAME + "/schema", json=data).json())
+
+
+    def replace_BM25(self, b=None, k1=None):
+        data = {
+            "replace-field-type": [
+                {
+                    'name': 'text_en',
+                    'class': 'solr.TextField',
+                    'positionIncrementGap': '100',
+                    'indexAnalyzer': {
+                        'tokenizer': {
+                            'class': 'solr.StandardTokenizerFactory'
+                        },
+                        'filters': [{
+                            'class': 'solr.StopFilterFactory',
+                            'words': 'lang/stopwords_en.txt',
+                            'ignoreCase': 'true'
+                        }, {
+                            'class': 'solr.LowerCaseFilterFactory'
+                        }, {
+                            'class': 'solr.EnglishPossessiveFilterFactory'
+                        }, {
+                            'class': 'solr.KeywordMarkerFilterFactory',
+                            'protected': 'protwords.txt'
+                        }, {
+                            'class': 'solr.PorterStemFilterFactory'
+                        }]
+                    },
+                    'similarity': {
+                        'class': 'solr.BM25SimilarityFactory',
+                        'b': str(b),
+                        'k1': str(k1)
+                    },
+                    'queryAnalyzer': {
+                        'tokenizer': {
+                            'class': 'solr.StandardTokenizerFactory'
+                        },
+                        'filters': [{
+                            'class': 'solr.SynonymGraphFilterFactory',
+                            'expand': 'true',
+                            'ignoreCase': 'true',
+                            'synonyms': 'synonyms.txt'
+                        }, {
+                            'class': 'solr.StopFilterFactory',
+                            'words': 'lang/stopwords_en.txt',
+                            'ignoreCase': 'true'
+                        }, {
+                            'class': 'solr.LowerCaseFilterFactory'
+                        }, {
+                            'class': 'solr.EnglishPossessiveFilterFactory'
+                        }, {
+                            'class': 'solr.KeywordMarkerFilterFactory',
+                            'protected': 'protwords.txt'
+                        }, {
+                            'class': 'solr.PorterStemFilterFactory'
+                        }]
+                    }
+                }, {
+                    'name': 'text_ru',
+                    'class': 'solr.TextField',
+                    'positionIncrementGap': '100',
+                    'analyzer': {
+                        'tokenizer': {
+                            'class': 'solr.StandardTokenizerFactory'
+                        },
+                        'filters': [{
+                            'class': 'solr.LowerCaseFilterFactory'
+                        }, {
+                            'class': 'solr.StopFilterFactory',
+                            'format': 'snowball',
+                            'words': 'lang/stopwords_ru.txt',
+                            'ignoreCase': 'true'
+                        }, {
+                            'class': 'solr.SnowballPorterFilterFactory',
+                            'language': 'Russian'
+                        }]
+                    },
+                    'similarity': {
+                        'class': 'solr.BM25SimilarityFactory',
+                        'b': str(b),
+                        'k1': str(k1)
+                    },
+                }, {
+                    'name': 'text_de',
+                    'class': 'solr.TextField',
+                    'positionIncrementGap': '100',
+                    'analyzer': {
+                        'tokenizer': {
+                            'class': 'solr.StandardTokenizerFactory'
+                        },
+                        'filters': [{
+                            'class': 'solr.LowerCaseFilterFactory'
+                        }, {
+                            'class': 'solr.StopFilterFactory',
+                            'format': 'snowball',
+                            'words': 'lang/stopwords_de.txt',
+                            'ignoreCase': 'true'
+                        }, {
+                            'class': 'solr.GermanNormalizationFilterFactory'
+                        }, {
+                            'class': 'solr.GermanLightStemFilterFactory'
+                        }]
+                    },
+                    'similarity': {
+                        'class': 'solr.BM25SimilarityFactory',
+                        'b': str(b),
+                        'k1': str(k1)
+                    },
+                }
+            ]
+        }
+
+        print(requests.post(self.solr_url + CORE_NAME + "/schema", json=data).json())
+
+
+if __name__ == "__main__":
+    d = open('train.json')
+    collection = json.load(d)
+    i = Indexer()
+    i.do_initial_setup()
+
+    i.replace_BM25(b=0.8, k1=1.4)
+    
+    i.add_fields()
+    i.replace_fields()
+    i.create_documents(collection)
+    
+
+
